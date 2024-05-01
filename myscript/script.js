0d0921e689d481874b1960f64f165043f54ed1f6
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-01 18:01:19 +0200

    parte 1 javascript

---
+++ myscript/script.js
@@ -0,0 +1,18 @@
+// applico il metodo trovato online per invertire la parola inserita
+function verifica() {
+    // lego la parola
+    let parola = document.getElementById("parola").value;
+    // inverto la parola
+    let reversed = parola.split('').reverse().join('');
+    // ciclo per capire se la parola e palindroma
+    if (parola == 0) {
+        document.getElementById("risultato").innerHTML = "inserisci prima una parola"
+
+    }
+    else if (parola == reversed) {
+        document.getElementById("risultato").innerHTML = "ze palindroma"
+    }
+    else (
+        document.getElementById("risultato").innerHTML = "no"
+    )
+}
